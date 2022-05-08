fc686ebffe281d8f547f5be0ce53e57b8f671309
Benjamin Oudejans <benjamin.oudejans@gmail.com>
2022-05-08 17:26:00 +0200

    Animate.py doet nu dingen in een aparte folder

--- animate.py
+++ animate.py
@@ -4,6 +4,7 @@ from main import *
 import imageio
 from datetime import datetime
 import webbrowser
+import os
 
 
 if __name__ == "__main__":
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     r_min = 0.02
     r = 0.5
     S = np.array([[1, 1, -2, 0, 0]] * N)
-    u_c = np.array(np.linspace(0,1,N+1))
+    u_c = np.array(np.linspace(0, 1, N + 1))
     H = 1
     J_hat = H  # set equal as in paper
 
@@ -25,14 +26,25 @@ if __name__ == "__main__":
     filenames = []
     plt.figure(figsize=(8, 6), dpi=80)
 
-    while r > r_min and ii<10000:
+    now = datetime.now()  # current date and time
+    time = now.strftime("%H.%M.%S")
+    date = now.strftime(r"%d-%m-%Y")
+    folder = f"animations/{date}/"
+    # folder = ""
+    root_dir = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/") + "/"
+    try:
+        os.makedirs(root_dir + folder)
+    except FileExistsError:
+        pass
+
+    while r > r_min and ii < 10000:
         J_tildes = compute_all_J_tildes(S, u_c, r)
         bqm = create_bqm(H, J_hat, J_tildes, boundary_condition="D")
         sampleset = simulated_sample(bqm)
         # solver.sample(bqm)  # adjust this to the solver!
         a_min = compute_a_min(sampleset, u_c, r)
         new_Pi = Pi_functional(S, a_min)
-        print(Pi_min,new_Pi,r)
+        print(Pi_min, new_Pi, r)
         if Pi_min < new_Pi:
             u_c = a_min
         else:
@@ -47,20 +59,22 @@ if __name__ == "__main__":
         plt.subplot(212)
         plt.title(f"Pi_min: {Pi_min}, new_Pi: {new_Pi}")
 
-        show_bqm_graph(bqm,False)
+        show_bqm_graph(bqm, False)
         # plt.subplot(1)
-        plt.savefig(f"out{ii}.png")
-        filenames.append(f"out{ii}.png")
+        filename = folder + f"out{ii}.png"
+        plt.savefig(filename)
+        filenames.append(filename)
         # plt.show()
-        ii+=1
+        ii += 1
         plt.clf()
     # plt.show()
     images = []
     for filename in filenames:
         images.append(imageio.imread(filename))
-    now = datetime.now()  # current date and time
 
-    time = now.strftime("%H.%M.%S")
-
-    imageio.mimsave(f"movie_{time}.gif", images, duration=0.005)
-    webbrowser.open(f"movie_{time}.gif")
+    movie_filename = folder + f"movie_{time}.gif"
+    imageio.mimsave(movie_filename, images, duration=0.005)
+    for filename in filenames:
+        os.remove(root_dir + filename)
+    print("file://" + root_dir + movie_filename)
+    webbrowser.open("file://" + root_dir + movie_filename)
